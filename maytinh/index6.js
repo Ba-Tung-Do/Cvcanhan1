c7ba3862c70e0bc5c86b39577140a94f28b34b6f
tung <email@example.com>
2025-03-11 14:37:13 +0700

    maytinhdongian

---
+++ maytinh/index6.js
@@ -0,0 +1,25 @@
+function phepcong() {
+    sothunhat = parseFloat(document.getElementById("thefirst").value);
+    sothuhai = parseFloat(document.getElementById("thesecond").value);
+    let tong = sothunhat + sothuhai;
+    document.getElementById("result").innerHTML = ("result: " + tong);
+}
+
+function pheptru() {
+    sothunhat = parseFloat(document.getElementById("thefirst").value);
+    sothuhai = parseFloat(document.getElementById("thesecond").value);
+    let hieu = sothunhat - sothuhai;
+    document.getElementById("result").innerHTML = "result :" + hieu;
+}
+function phepnhan() {
+    sothunhat = parseFloat(document.getElementById("thefirst").value);
+    sothuhai = parseFloat(document.getElementById("thesecond").value);
+    let nhan = sothunhat * sothuhai;
+    document.getElementById("result").innerHTML = "result :" + nhan;
+}
+function phepchia() {
+    sothunhat = parseFloat(document.getElementById("thefirst").value);
+    sothuhai = parseFloat(document.getElementById("thesecond").value);
+   let  chia = sothunhat / sothuhai;
+    document.getElementById("result").innerHTML = "result :" + chia;
+}
